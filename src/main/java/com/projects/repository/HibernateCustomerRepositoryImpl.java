b4490977b82af7140e95979e0aeaab6681905011
kthong3 <kthongsakounh@gmail.com>
2018-09-12 15:24:37 -0500

    create HibernateCustomerRepositoryImpl class for finding all customers

---
+++ src/main/java/com/projects/repository/HibernateCustomerRepositoryImpl.java
@@ -0,0 +1,17 @@
+package com.projects.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.projects.model.Customer;
+
+public class HibernateCustomerRepositoryImpl {
+    public List<Customer> findAll() {
+        List<Customer> customers = new ArrayList<Customer>();
+        Customer customer = new Customer();
+        customer.setFirstName("bryan");
+        customer.setLastName("hansen");
+
+        customers.add(customer);
+        return customers;
+    }
+}
